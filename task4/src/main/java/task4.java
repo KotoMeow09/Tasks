9204dc248ee5a20fb0ce3c2f19fd9e9999cfe683
KotoMeow09 <team.killers.kotyra@gmail.com>
2026-06-19 22:04:55 +0300

    Добавил задание 4

---
+++ task4/src/main/java/task4.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class task4 {
+    public static void main(String[] args) throws IOException {
+        String path = Files.readString(Paths.get(args[0]));
+        String[] numbers = new String(path).split("\\s+");
+        int [] n = new int[numbers.length];
+
+            for (int i = 0; i < numbers.length; i++){
+                n[i] = Integer.parseInt(numbers[i]);}
+
+            if (n.length == 0){
+                System.out.println(0);
+                return;}
+
+                java.util.Arrays.sort(n);
+                int mediana = n[n.length / 2];
+
+                    long moves = 0;
+                    for(int pric : n) {
+                    moves += Math.abs(pric - mediana);}
+
+                        if (moves <= 20){
+                            System.out.println(moves);
+
+                        }else {
+                            System.out.println("За 20 ходов невозможно");}
+    }
+}
